492cbb7362b308240a398b510ca84a35a0b8990a
fjmedina18 <medinafrancisco.m@gmail.com>
2022-08-18 23:04:02 -0400

    Save Local Vers 2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import threading
 import conf
 import requests
 import json
@@ -30,24 +31,23 @@ def top_system():
     }
     requests.packages.urllib3.disable_warnings()
     respuesta = requests.get(sandbox+"/api/node/mo/topology/pod-1.json?query-target=children&target-subtree-class=fabricNode", headers=cabecera, cookies= galleta, verify=False)
-    print(respuesta.json())
+    #print(respuesta.json())
+    print("Equipo " + respuesta.json()["imdata"][0]["fabricNode"]['attributes']['name'])
     print("Direccion IP " + respuesta.json()["imdata"][0]["fabricNode"]['attributes']['address'])
     print("Estado " + respuesta.json()["imdata"][0]["fabricNode"]['attributes']['adSt'])
-    #print(respuesta.json())["imdata"][0]["fabricNode"]
-top_system()
-
-
-def top_system():
-    cabecera = {
-        "Content-Type": "application/json"
-    }
-    galleta = {
-        "APIC-Cookie": obtener_token(conf.usuario, conf.clave)
-    }
-    requests.packages.urllib3.disable_warnings()
-    respuesta = requests.get(sandbox+"/api/node/mo/topology/pod-1/node-101.json", headers=cabecera, cookies= galleta, verify=False)
-    print("Equipo " +respuesta.json()["imdata"][0]["fabricNode"]['attributes']['name'])
-    print("Direccion IP " + respuesta.json()["imdata"][0]["fabricNode"]['attributes']['address'])
-    print("Estado " + respuesta.json()["imdata"][0]["fabricNode"]['attributes']['adSt'])
-    #print(respuesta.json())["imdata"][0]["fabricNode"]
+    print()
+    print("Equipo " +respuesta.json()["imdata"][1]["fabricNode"]['attributes']['name'])
+    print("Direccion IP " + respuesta.json()["imdata"][1]["fabricNode"]['attributes']['address'])
+    print("Estado " + respuesta.json()["imdata"][1]["fabricNode"]['attributes']['adSt'])
+    print()
+    print("Equipo " + respuesta.json()["imdata"][2]["fabricNode"]['attributes']['name'])
+    print("Direccion IP " + respuesta.json()["imdata"][2]["fabricNode"]['attributes']['address'])
+    print("Estado " + respuesta.json()["imdata"][2]["fabricNode"]['attributes']['adSt'])
+    print()
+    print("Equipo " + respuesta.json()["imdata"][3]["fabricNode"]['attributes']['name'])
+    print("Direccion IP " + respuesta.json()["imdata"][3]["fabricNode"]['attributes']['address'])
+    print("Estado " + respuesta.json()["imdata"][3]["fabricNode"]['attributes']['adSt'])
+    print()
+    t = threading.Timer (300, top_system)
+    t.start()
 top_system()
